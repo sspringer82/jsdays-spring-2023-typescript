90905b0ada242b84608d5e8e097b47e84363a2df
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2023-03-20 12:18:15 +0100

    extend config

--- node-ts-app/dist/index.js
+++ node-ts-app/dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
 const myName = 'Basti';
 console.log('Hallo ' + myName);
+class Person {
+    constructor(firstName) {
+        this.firstName = firstName;
+    }
+    greet(what) {
+        return `${what} ${this.firstName}`;
+    }
+}
+const klaus = new Person('Klaus');
 //# sourceMappingURL=index.js.map
--- node-ts-app/src/index.ts
+++ node-ts-app/src/index.ts
@@ -1,3 +1,13 @@
 const myName: string = 'Basti';
 
 console.log('Hallo ' + myName);
+
+class Person {
+  constructor(private readonly firstName: string) {}
+
+  greet(what: string): string {
+    return `${what} ${this.firstName}`;
+  }
+}
+
+const klaus = new Person('Klaus');
